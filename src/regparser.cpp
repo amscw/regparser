f4d927927eaea9e1d378b1be0ec879aa8df651f5
alex <moskwin.a.va@gmail.com>
2017-11-11 14:57:53 +0300

    добавлены файлы проекта-затычки

---
+++ src/regparser.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : regparser.cpp
+// Author      : moskvin
+// Version     :
+// Copyright   : elins
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hello, fucking world!" << endl; // prints hello, fucking world!
+	return 0;
+}
