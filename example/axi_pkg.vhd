cd41edc4577320e20b63b1a773602079979b091f
user <user@linux-vm1.elins.internal>
2017-11-21 20:02:39 +0300

    добавил новый образец vhd-файла

--- example/axi_pkg.vhd
+++ example/axi_pkg.vhd
@@ -1,136 +1,136 @@
+-----------------------------------------------------------------------------------------
+-- Project : FHSS-modem
+-- Company: АО "НТЦ Элинс"
+-- Author :Мельников Александр Юрьевич
+-- Date : 2017-11-30
+-- File : axi_pkg.vhd
+-- Design: FHSS-modem
+-----------------------------------------------------------------------------------------
+-- Description : package с регистрами axi
+-----------------------------------------------------------------------------------------
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
+use ieee.math_real.all;
 
-package axi_pkg is
-	constant OPT_MEM_ADDR_BITS : integer := 15;
+package Axi_pkg is
+	-- Количество регистров AXI
+	constant REG_CNT           : integer := 256;
+	constant OPT_MEM_ADDR_BITS : integer := integer(ceil(log2(real(REG_CNT*4))));
 
-	-- Регистры MLIP и DMA --
 	-- регистр включения/выключения заворотки MLIP
-	constant ADDR_REG_MLIP_LOOP      : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0000";
+	constant ADDR_REG_MLIP_LOOP                  : integer := 0;
 	-- регистр управления DMA (0 бит - запуск DMA MM2P)
-	constant ADDR_REG_DMA_CR         : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0004";
+	constant ADDR_REG_DMA_CR                     : integer := 1;
 	-- регистр статуса DMA (1 бит - прерывание intr_rx)
-	constant ADDR_REG_DMA_SR         : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0008";
+	constant ADDR_REG_DMA_SR                     : integer := 2;
 	-- регистр включения прерываний DMA (1 бит - прерывание intr_rx)
-	constant ADDR_REG_DMA_IR         : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"000C";
+	constant ADDR_REG_DMA_IR                     : integer := 3;
 	-- адрес источника
-	constant ADDR_REG_DMA_SA         : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0010";
+	constant ADDR_REG_DMA_SA                     : integer := 4;
 	-- адрес назначения
-	constant ADDR_REG_DMA_DA         : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0014";
+	constant ADDR_REG_DMA_DA                     : integer := 5;
 	-- длина массива данных DMA для источника 
-	constant ADDR_REG_DMA_SL         : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0018";
+	constant ADDR_REG_DMA_SL                     : integer := 6;
 	-- длина массива данных DMA для получателя (read only) 
-	constant ADDR_REG_DMA_DL         : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"001C";
+	constant ADDR_REG_DMA_DL                     : integer := 7;
 	-- регистр статуса MLIP (0-ой бит - intr_tx)
-	constant ADDR_REG_MLIP_SR        : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0020";
+	constant ADDR_REG_MLIP_SR                    : integer := 8;
 	-- регистр включения прерываний MLIP (0-ой бит - intr_tx)
-	constant ADDR_REG_MLIP_IR        : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0024";
+	constant ADDR_REG_MLIP_IR                    : integer := 9;
 	-- регистр rst MLIP (0-ой бит - Mlip_rst_o)
-	constant ADDR_REG_MLIP_RST       : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0028";
+	constant ADDR_REG_MLIP_RST                   : integer := 10;
 	-- регистр ce MLIP (0-ой бит - Mlip_ce_o)
-	constant ADDR_REG_MLIP_CE        : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"002C";
+	constant ADDR_REG_MLIP_CE                    : integer := 11;
 	-- Включить подсчет битых пакетов (0-ой бит - Mlip_cntcrc_on)
-	constant ADDR_REG_MLIP_CNTCRC_ON : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0030";
-	-- Счетчик битых пакетов MLIP (16 младших бит) (read only)
-	constant ADDR_REG_MLIP_CNTCRC    : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0034";
+	constant ADDR_REG_MLIP_CNTCRC_ON             : integer := 12;
+	-- Включить поле размер пакета в пакете MLIP
+	constant ADDR_REG_MLIP_EN_SIZE               : integer := 13;
+	-- Включить поле CRC пакета в пакете MLIP
+	constant ADDR_REG_MLIP_EN_CRC                : integer := 14;
+	-- Порог совпадения преамбулы MLIP
+	constant ADDR_REG_MLIP_MATCH_CNT             : integer := 15;
+	-- Количество неверных CRC в принятых пакетах
+	constant ADDR_REG_MLIP_CNTCRC                : integer := 16;
+	-- Размер пакетов MLIP при выключенном поле размера пакета
+	constant ADDR_REG_MLIP_SIZE_PACK             : integer := 17;
+	-- Сброс Канального и Физического уровня
+	constant ADDR_REG_M_RST                      : integer := 18;
+	-- Конфигурация Master/Slave (1 - Master, 0 - Slave)
+	constant ADDR_REG_M_MASTER                   : integer := 19;
+	-- Режим работы (0 - Односторонний, 1 - Симплексный)
+	constant ADDR_REG_M_MODE                     : integer := 20;
+	-- Регистры DLink Level
+	-- Размер пакета синхронизации
+	constant ADDR_REG_DLINK_SYNCH_FRAME_SIZE     : integer := 21;
+	-- Размер пакета Подтверждения
+	constant ADDR_REG_DLINK_ACK_FRAME_SIZE       : integer := 22;
+	-- Размер пакета Данных
+	constant ADDR_REG_DLINK_DATA_FRAME_SIZE      : integer := 23;
+	-- Регистры PHY Level
+	-- DBG --
+	-- Запись 1 - меняет частоту при выключенном hopper 
+	constant ADDR_REG_PHY_DBG_SEL_FREQ           : integer := 24;
+	-- Проскальзывание Гарднера Вперед в сэмплах
+	constant ADDR_REG_PHY_DBG_GARD_SLIP_FW       : integer := 25;
+	-- Проскальзывание гарднера назад в сэмплах
+	constant ADDR_REG_PHY_DBG_GARD_SLIP_BW       : integer := 26;
+	-- подключение для тестирования valid и sync от мастера к слэйву и обратно
+	constant ADDR_REG_PHY_DBG_HALFDUPLEX         : integer := 27;
+	-- Включение хоппера
+	constant ADDR_REG_PHY_M_EN_HOPPER            : integer := 28;
+	-- Степень скользящего среднего в блоке PowerMeter
+	constant ADDR_REG_PHY_M_PWRMET_AV_SIZE       : integer := 29;
+	-- Включение цифровой заворотки модема
+	constant ADDR_REG_PHY_M_LOOP                 : integer := 30;
+	-- Включение блока AFC
+	constant ADDR_REG_PHY_M_EN_AFC               : integer := 31;
+	-- Mu_p для Гарднера
+	constant ADDR_REG_PHY_M_GARD_MU_P            : integer := 32;
+	-- Заморозка Гарднера
+	constant ADDR_REG_PHY_M_GARD_FRZ             : integer := 33;
+	-- Порог для эквалайзера
+	constant ADDR_REG_PHY_M_TEQ_TH               : integer := 34;
+	-- Коэффициент для петлевого фильтра AFC
+	constant ADDR_REG_PHY_M_AFC_SCALE            : integer := 35;
+	-- Коэффициент расширения полосы ППРЧ
+	constant ADDR_REG_PHY_M_BW_FACTOR            : integer := 36;
+	-- Смещение Полосы ППРЧ относительно несущей частоты
+	constant ADDR_REG_PHY_M_DC_OFFSET            : integer := 37;
+	-- Включение проскальзывания в hopper
+	constant ADDR_REG_PHY_M_HOP_EN_SLIP          : integer := 38;
+	-- Параметр инициализации вихря Мерсена
+	constant ADDR_REG_PHY_M_HOP_SEED             : integer := 39;
+	-- Период ожидания после проскальзывания
+	constant ADDR_REG_PHY_M_HOP_TIMEOUT_AFT_SLIP : integer := 40;
+	-- Период проверки ошибки частотной синхронизации с блока PowerMeter
+	constant ADDR_REG_PHY_M_HOP_EST_PRD          : integer := 41;
+	-- Начальная оценка синхронизации после проскальзывания
+	constant ADDR_REG_PHY_M_HOP_EST_START        : integer := 42;
+	-- Порог ошибки частной синхронизации
+	constant ADDR_REG_PHY_M_HOP_ERR_TH           : integer := 43;
+	-- Максимальное значение оценки частотной синхронизации
+	constant ADDR_REG_PHY_M_HOP_EST_MAX          : integer := 44;
+	-- Размер таблицы частот
+	constant ADDR_REG_PHY_M_HOP_LOOKUP_SIZE      : integer := 45;
+	-- Включить сброс синхронизации от DlinkLayer
+	constant ADDR_REG_PHY_M_HOP_EN_LINK          : integer := 46;
+	-- Включение управления аналоговым усилителем передатчика
+	constant ADDR_REG_PHY_IC_EN_AMP              : integer := 47;
+	-- Включение управления аналоговым усилителем приемника
+	constant ADDR_REG_PHY_IC_EN_LNA              : integer := 48;
+	-- Включение ручного режима управления аттенюатором 
+	constant ADDR_REG_PHY_IC_ATT_MODE            : integer := 49;
+	-- Занчение аттенюации при ручном управлении
+	constant ADDR_REG_PHY_IC_ATT_VALUE           : integer := 50;
+	-- Состояние частотной синхронизации
+	constant ADDR_REG_PHY_LOCK_DET               : integer := 51;
+	-- Нижний порог петли гистерезиса для AttCtrl
+	constant ADDR_REG_PHY_IC_ATT_THLOW           : integer := 52;
+	-- Верхний порог петли гистерезиса для AttCtrl
+	constant ADDR_REG_PHY_IC_ATT_THHIGH          : integer := 53;
+	-- Период обновления значения усиления в AD9361
+	constant ADDR_REG_PHY_IC_ATT_UPD_CNT         : integer := 54;
 
-	-- Регистры модема --
-	-- rst MODEM (0-ой бит - m_rst_o)
-	constant ADDR_REG_M_RST          : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0038";
-	-- цифровая заворотка FPGA loop_back MODEM (0-ой бит - m_loop_back_o)
-	constant ADDR_REG_M_LOOP         : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"003C";
-	-- look_detect MODEM (0-ой бит - m_look_detect_i) - read only
-	constant ADDR_REG_M_LOOK_DET     : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0040";
-	-- Включение модулятора m_en_mod_o MODEM (0-ой бит - m_en_mod_o) 
-	constant ADDR_REG_M_EN_MOD       : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0044";
-	-- Включение модулятора m_en_demod_o MODEM (0-ой бит - m_en_demod_o) 
-	constant ADDR_REG_M_EN_DEMOD     : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0048";
-	-- Включение hopper m_en_hopper_o MODEM (0-ой бит - m_en_hopper_o) 
-	constant ADDR_REG_M_EN_HOPPER    : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"004C";
-	-- Ручное переклюдчение частоты m_select_freq_o MODEM (строб по моменту записи в 0-ой бит единицы) 
-	constant ADDR_REG_M_SEL_FREQ     : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0050";
-	-- Инициализация Вихря Мерсена m_seed (32 бита) 
-	constant ADDR_REG_M_SEED         : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0054";
-	-- Глубина усреднения ошибки гарднера m_average_size_o (16 младших бит) 
-	--constant ADDR_REG_M_SOFT_CNT     : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0058";
-	-- Период проскальзывания по частоте RX m_decision_prd_o (16 младших бит) 
-	constant ADDR_REG_M_DEC_PRD      : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"005C";
-	-- Граничное значение ошибки Гарднера (16 младших бит) 
-	--constant ADDR_REG_M_SOFT_RANGE   : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0060";
-	-- Граничное значение ошибки Гарднера (16 младших бит) 
-	--constant ADDR_REG_M_SOFT_MAX     : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0064";
-	-- Граничное значение ошибки Гарднера (16 младших бит) 
-	--constant ADDR_REG_M_SOFT_TH      : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0068";
-	-- размерность сетки частот m_lookup_size_o (16 младших бит) 
-	constant ADDR_REG_M_LOOKUP_SIZE  : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"006C";
-	--  (0-ой бит - m_ad_enable_o) 
-	constant ADDR_REG_M_AD_EN        : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0070";
-	-- (0-ой бит - m_ad_txnrx_o)  
-	constant ADDR_REG_M_AD_TXNRX     : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0074";
-	-- (0-ой бит - m_ad_inc_o)  
-	--constant ADDR_REG_M_AD_INC       : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0078";
-	-- (0-ой бит - m_ad_dec_o)  
-	--constant ADDR_REG_M_AD_DEC       : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"007C";
-	-- (0-ой бит - m_ad_bust_conf_o)  
-	--constant ADDR_REG_M_AD_BUST_CONF : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0080";
-	-- (7 младших бит - m_ad_light_i) (read only)  
-	constant ADDR_REG_M_AD_LIGHT     : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0084";
-	-- (0-ой бит - cic_en)  
-	--constant ADDR_REG_M_EN_CIC       : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0088";
-	-- (0-ой бит - afc_en)  
-	--constant ADDR_REG_M_EN_AFC       : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"008C";
-	-- (0-ой бит - agc_en)  
-	--constant ADDR_REG_M_EN_AGC       : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0090";
-	-- (0-ой бит - agc_en)  
-	--constant ADDR_REG_M_EN_FARROW    : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0094";
-	-- (0-ой бит - agc_en)  
-	--constant ADDR_REG_M_EN_TEQ       : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0098";
-	-- (0-ой бит - agc_en)  
-	--constant ADDR_REG_M_EN_RRC       : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"009C";
-	-- (0-ой бит - agc_en)  
-	constant ADDR_REG_M_TYPE_SYNC    : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00A0";
-	-- (0-ой бит - agc_en)  
-	constant ADDR_REG_M_TEQ_TH       : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00A4";
-	-- (0-ой бит - agc_en)  
-	constant ADDR_REG_M_PWR_AV_SIZE  : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00A8";
-	-- (0-ой бит - agc_en)  
-	constant ADDR_REG_M_SCALE_RANGE  : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00AC";
-	-- (0-ой бит - agc_en)  
-	constant ADDR_REG_M_OFFSET       : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00B0";
-	-- (16 младших бит - gardner_mu_p) 
-	constant ADDR_REG_M_GARD_MU_P    : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00B4";
-	-- (16 младших бит - gardner_start) 
-	constant ADDR_REG_M_GARD_FRZ     : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00B8";
-	-- (0-ой бит - slip_on)  
-	constant ADDR_REG_M_HOPPER_SLIP  : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00BC";
-	-- (16 младших бит - cnt_sma) 
-	constant ADDR_REG_M_CNT_SMA      : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00C0";
-	-- (16 младших бит - cnt_sma) 
-	constant ADDR_REG_M_AGC_GAIN     : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00C4";
-	-- счетчик прерываний TX
-	constant ADDR_REG_MLIP_CNT_TX    : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00C8";
-	-- Счетчик рперываний RX
-	constant ADDR_REG_MLIP_CNT_RX    : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00CC";
-	-- Ручное проскальзывание вперед
-	constant ADDR_REG_M_GARD_SLIP_FW : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00D0";
-	-- Ручное проскальзывание назад
-	constant ADDR_REG_M_GARD_SLIP_BW : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00D4";
-	--constant ADDR_REG_M_ATT          : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00D8";
-	--constant ADDR_REG_M_AFC_SCALE    : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00DC";
-	constant ADDR_REG_MLIP_EN_SIZE   : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00E0";
-	constant ADDR_REG_MLIP_EN_CRC    : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00E4";
-	constant ADDR_REG_MLIP_MATCH_CNT : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00E8";
-	constant ADDR_REG_MLIP_SIZE_PACK : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00EC";
-	--constant ADDR_REG_M_ATT_MANUAL   : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00F0";
-	--constant ADDR_REG_M_ATT_AVSIZE   : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00F4";
-	--constant ADDR_REG_M_ATT_THLOW    : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00F8";
-	--constant ADDR_REG_M_ATT_THHIGH   : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"00FC";
-	--constant ADDR_REG_M_ATT_UPDCNT   : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0100";
-	constant ADDR_REG_M_TEQ_PH       : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0104";
-	constant ADDR_REG_CFG_MASTER     : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0108";
-	constant ADDR_REG_EN_HALFDUPLEX  : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"010C";
-	constant ADDR_REG_BIT2TX         : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0110";
-	constant ADDR_REG_BIT2RX         : std_logic_vector(OPT_MEM_ADDR_BITS downto 0) := x"0114";
-
-end package axi_pkg;
+end package Axi_pkg;
